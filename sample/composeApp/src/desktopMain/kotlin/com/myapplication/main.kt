ffe17ccc78e3c84f50e10fb714b5d930d1b01619
Cavin <cavinmacwan1@gmail.com>
2025-07-16 23:54:49 +0530

    init commit

---
+++ sample/composeApp/src/desktopMain/kotlin/com/myapplication/main.kt
@@ -0,0 +1,14 @@
+package com.myapplication
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.myapplication.App
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "myapplication",
+    ) {
+        App()
+    }
+}
---
+++ sample/composeApp/src/iosMain/kotlin/com/myapplication/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.myapplication
+
+import androidx.compose.ui.window.ComposeUIViewController
+import com.myapplication.App
+
+fun MainViewController() = ComposeUIViewController { App() }
